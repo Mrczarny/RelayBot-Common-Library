087cd98ac9780a335cad33931de708e3b77ba08d
VuurtorenEDM <everett.munion@student.nhlstenden.com>
2024-11-20 21:27:36 +0100

    Changed Motor Controlls to Operate via one PWM Pin and one Digital Pin, added Left and Right Motor Speed Functions

--- src/motors.cpp
+++ src/motors.cpp
@@ -15,53 +15,75 @@ void Motors::setup(
 
 
 void Motors::forward() {
-  analogWrite(_leftMotorForwardPin, _maxLeftForwardSpeed);
-  analogWrite(_leftMotorBackwardPin, 0);
-  analogWrite(_rightMotorForwardPin, _maxRightForwardSpeed);
-  analogWrite(_rightMotorBackwardPin, 0);
+  digitalWrite(_leftMotorDirectionPin, HIGH);      // direction of left motor
+  analogWrite(_leftMotorSpeedPin, _maxLeftForwardSpeed);   // speed of left motor 
+  digitalWrite(_rightMotorDirectionPin, HIGH);      // direction of right motor
+  analogWrite(_rightMotorSpeedPin, _maxRightForwardSpeed);  // speed of right motor 
+}
+
+void Motors::forward(int speed) {
+  digitalWrite(_leftMotorDirectionPin, HIGH);      // direction of left motor
+  analogWrite(_leftMotorSpeedPin, _maxLeftForwardSpeed*speed);   // speed of left motor 
+  digitalWrite(_rightMotorDirectionPin, HIGH);      // direction of right motor
+  analogWrite(_rightMotorSpeedPin, _maxRightForwardSpeed*speed);  // speed of right motor 
 }
 
 void Motors::backward() {
-  analogWrite(_leftMotorForwardPin, 0);
-  analogWrite(_leftMotorBackwardPin, _maxLeftBackwardSpeed);
-  analogWrite(_rightMotorForwardPin, 0);
-  analogWrite(_rightMotorBackwardPin, _maxRightBackwardSpeed);
+  analogWrite(_leftMotorDirectionPin, LOW);
+  analogWrite(_leftMotorSpeedPin, _maxLeftBackwardSpeed);
+  analogWrite(_rightMotorDirectionPin, LOW);
+  analogWrite(_rightMotorDirectionPin, _maxRightBackwardSpeed);
 }
 
 void Motors::zeroLeft() {
-  analogWrite(_leftMotorForwardPin, _maxLeftBackwardSpeed);
-  analogWrite(_leftMotorBackwardPin, 0);
-  analogWrite(_rightMotorForwardPin, 0);
-  analogWrite(_rightMotorBackwardPin, _maxRightBackwardSpeed);
+  analogWrite(_leftMotorDirectionPin, LOW);
+  analogWrite(_leftMotorSpeedPin, _maxLeftBackwardSpeed);
+  analogWrite(_rightMotorDirectionPin, HIGH);
+  analogWrite(_rightMotorSpeedPin, _maxRightForwardSpeed);
 }
 
 void Motors::zeroRight() {
-  analogWrite(_leftMotorForwardPin, 0);
-  analogWrite(_leftMotorBackwardPin, _maxLeftBackwardSpeed);
-  analogWrite(_rightMotorForwardPin, _maxRightBackwardSpeed);
-  analogWrite(_rightMotorBackwardPin, 0);
+  analogWrite(_leftMotorDirectionPin, HIGH);
+  analogWrite(_leftMotorSpeedPin, _maxLeftForwardSpeed);
+  analogWrite(_rightMotorDirectionPin, LOW);
+  analogWrite(_rightMotorSpeedPin, _maxRightBackwardSpeed);
 }
 
 void Motors::right() {
-  analogWrite(_leftMotorForwardPin, _maxLeftForwardSpeed);
-  analogWrite(_leftMotorBackwardPin, 0);
-  analogWrite(_rightMotorForwardPin, _maxRightForwardSpeed*0.7);
-  analogWrite(_rightMotorBackwardPin, 0);
+  analogWrite(_leftMotorDirectionPin, HIGH);
+  analogWrite(_leftMotorSpeedPin, _maxLeftForwardSpeed);
+  analogWrite(_rightMotorDirectionPin, HIGH);
+  analogWrite(_rightMotorDirectionPin, _maxRightForwardSpeed*0.7);
 }
 
 void Motors::left() {
-  analogWrite(_leftMotorForwardPin, _maxLeftForwardSpeed*0.7);
-  analogWrite(_leftMotorBackwardPin, 0);
-  analogWrite(_rightMotorForwardPin, _maxRightForwardSpeed);
-  analogWrite(_rightMotorBackwardPin, 0);
+  analogWrite(_leftMotorDirectionPin, HIGH);
+  analogWrite(_leftMotorSpeedPin, _maxLeftForwardSpeed*0.7);
+  analogWrite(_rightMotorDirectionPin, HIGH);
+  analogWrite(_rightMotorSpeedPin, _maxRightForwardSpeed);
 }
 
 void Motors::stop() {
-  analogWrite(_leftMotorForwardPin, 0);
-  analogWrite(_leftMotorBackwardPin, 0);
-  analogWrite(_rightMotorForwardPin, 0);
-  analogWrite(_rightMotorBackwardPin, 0);
+  analogWrite(_leftMotorDirectionPin, LOW);
+  analogWrite(_leftMotorSpeedPin, 0);
+  analogWrite(_rightMotorDirectionPin, LOW);
+  analogWrite(_rightMotorSpeedPin, 0);
 }
 
+void Motors::setLeftMotorSpeed(int speed){
+  if (speed >= 0 && speed <= 255){
+    _left_speed = speed;
+    analogWrite(_leftMotorSpeedPin, _left_speed);
+  }
+}
+
+void Motors::setRightMotorSpeed(int speed){
+  if (speed >= 0 && speed <= 255){
+    _right_speed = speed;
+    analogWrite(_rightMotorSpeedPin, _right_speed);
+  }
+}
+
+// correct all functions, and add set motor speed functions
 
 

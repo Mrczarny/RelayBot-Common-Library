ee55ad5f3788f09c776b6d43174fe6186b6cb173
mrczarny <j.adamski@zsebydgoszcz.onmicrosoft.com>
2024-11-25 22:16:59 +0100

    rotation as lib wip

--- include/rotation.h
+++ include/rotation.h
@@ -3,34 +3,33 @@
 
 #include "Arduino.h"
 
-class Rotation {
-    public:
-        Rotation(int leftMotorRotation, int rightMotorRotation):
-            _leftMotorRotationPin(leftMotorRotation),
-            _rightMotorRotationPin(rightMotorRotation)
+static int _leftCount;
+static int _rightCount;
+static int COUNT_INTERVAL;
 
-            {
-                pinMode(_leftMotorRotationPin, INPUT_PULLUP);
-                pinMode(_rightMotorRotationPin, INPUT_PULLUP);
-                attachInterrupt(digitalPinToInterrupt(_leftMotorRotationPin), leftRotationCheck, CHANGE);
-                attachInterrupt(digitalPinToInterrupt(_rightMotorRotationPin), rightRotationCheck, CHANGE);
-            }
-            void setup(
-                int _leftCount,
-                int _rightCount            
-            );
-            static void leftRotationCheck();
-            static void rightRotationCheck();
-        private:
-            int _leftMotorRotationPin;
-            int _rightMotorRotationPin;
-            int _leftCount = 0;
-            int _rightCount = 0;
-            const int COUNT_INTERVAL = 10;
+class Rotation
+{
+public:
+    Rotation(int leftMotorRotation, int rightMotorRotation)
+    {
+        _leftMotorRotationPin = leftMotorRotation;
+        _rightMotorRotationPin = rightMotorRotation;
+        _leftCount = 0;
+        _rightCount = 0;
+        COUNT_INTERVAL = 10;
+        pinMode(_leftMotorRotationPin, INPUT_PULLUP);
+        pinMode(_rightMotorRotationPin, INPUT_PULLUP);
+    }
+    void setup(
+        int leftCount,
+        int rightCount, 
+        int leftMotorRotationPin,
+        int rightMotorRotationPin);
+    void moveForwardFor(int distance);
+
+private:
+    int _leftMotorRotationPin;
+    int _rightMotorRotationPin;
 };
-
-
-
-
 
 #endif
--- src/rotation.cpp
+++ src/rotation.cpp
@@ -1,51 +1,63 @@
 #include "rotation.h"
+#include <motors.h>
 
+
+
+int _leftRotationPinInternal;
+int _rightRotationPinInternal;
 
 void leftRotationCheck();
 void rightRotationCheck();
 
-
 void Rotation::setup(
-    int _leftCount,
-    int _rightCount
-) {
-    _leftCount = _leftCount;
-    _rightCount = _rightCount;
+    int leftCount,
+    int rightCount, int leftMotorRotationPin, int rightMotorRotationPin)
+{
+    attachInterrupt(digitalPinToInterrupt(leftMotorRotationPin), leftRotationCheck, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(rightMotorRotationPin), rightRotationCheck, CHANGE);
+    _leftCount = leftCount;
+    _rightCount = rightCount;
+    _leftRotationPinInternal = _leftMotorRotationPin;
+    _rightRotationPinInternal = _rightMotorRotationPin;
+    
 }
 
 
-void Rotation::leftRotationCheck(){
+
+static void leftRotationCheck()
+{
+    Serial.println("leftRotationCheck");
     static unsigned long timer;
     static bool lastState;
     noInterrupts();
     if (millis() > timer)
     {
-        bool state = digitalRead(_leftMotorRotationPin);
-            if(state != lastState)
-            {
-                _leftCount++;
-                lastState = state;
-            }
+        bool state = digitalRead(_leftRotationPinInternal);
+        if (state != lastState)
+        {
+            _leftCount++;
+            lastState = state;
+        }
         timer = millis() + COUNT_INTERVAL;
     }
     interrupts();
 }
 
-
-void Rotation::rightRotationCheck(){
+static void rightRotationCheck()
+{
     static unsigned long timer;
     static bool lastState;
     noInterrupts();
     if (millis() > timer)
     {
-        bool state = digitalRead(_rightMotorRotationPin);
-            if(state != lastState)
-            {
-                _leftCount++;
-                lastState = state;
-            }
+        bool state = digitalRead(_rightRotationPinInternal);
+        if (state != lastState)
+        {
+            _rightCount++;
+
+            lastState = state;
+        }
         timer = millis() + COUNT_INTERVAL;
     }
     interrupts();
 }
-

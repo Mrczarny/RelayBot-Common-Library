7ca2c479ca8ecab508c003276a774a7987c7193b
Stefan1711UNI <stefan.sonderling1@student.nhlstenden.com>
2024-11-27 13:26:34 +0100

    Finished setup

--- include/gripper.h
+++ include/gripper.h
@@ -5,9 +5,10 @@
 
 class Gripper {
     public:
-    Gripper(int ServoPin):
-        _ServoPin(ServoPin)
-    
+    Gripper(int ServoPin, int OPENGRIPPER, int CLOSEGRIPPER):
+        _ServoPin(ServoPin),
+        _OpenGripper(OPENGRIPPER),  //My values were 2
+        _CloseGripper(CLOSEGRIPPER) //And 1 for closed
     {
         pinMode(_ServoPin, INPUT);
 
@@ -15,6 +16,8 @@ class Gripper {
     void gripper(int position);
     private:
     int _ServoPin;
+    const int _OpenGripper;
+    const int _CloseGripper;
 };
 
 #endif
